05a87901ffa954adc7a687292ca26ada4c776139
Marwan Yousry <myousry.dev@gmail.com>
2019-10-23 15:05:05 +0200

    add animate.css file to project install animate css npm package apply animation on side menu elements , home page cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -9,14 +9,22 @@ import { HomePage } from '../pages/home/home';
 })
 export class MyApp {
   rootPage:any = HomePage;
-
+  itemList: string[] = [];
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+    this.itemList = ['Home', 'Profile', 'Orders', 'About', 'Contact us', 'Contact us', 'Contact us', 'Contact us'];
+    
+
+    
+  }
+  ionViewWillEnter(){
+    
   }
 }
 
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AnimationBuilder, AnimationMode } from 'css-animator/builder';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  @ViewChild('card1') firstCard: ElementRef;
+  @ViewChild('card2') secondCard: ElementRef;
+  @ViewChild('card3') thirdCard: ElementRef;
 
+  card: string[];
   constructor(public navCtrl: NavController) {
+    this.card = ['Card 1', 'Card 2', 'Card 3', 'Card 4', 'Card 5'];
+  }
 
+  ionViewDidLoad(){
+    let animater = new AnimationBuilder();
+    animater.pin = false;
+    animater.duration = 1000;
+
+    animater.setType('fadeInLeft').show(this.firstCard.nativeElement).then(()=>{
+
+    });
+    animater.setType('fadeInLeft').show(this.secondCard.nativeElement).then(()=>{
+
+    });
+    animater.setType('fadeInLeft').show(this.thirdCard.nativeElement).then(()=>{
+
+    });
   }
 
 }
